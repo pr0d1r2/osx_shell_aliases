aa3142ec2f9f263f5be99260e83ad74355e199d7
Marcin Nowicki <pr0d1r2@gmail.com>
2017-02-08 07:01:52 +0100

    Also update project if existing

--- sync_hammerspoon.sh
+++ sync_hammerspoon.sh
@@ -1,4 +1,15 @@
 function sync_hammerspoon() {
+  if [ -d ~/projects/dothammerspoon ]; then
+    echorun cd ~/projects/dothammerspoon || return $?
+    echorun git pull --recurse-submodules || return $?
+    echorun git submodule update --recursive --remote || return $?
+    git status | grep -q 'Your branch is up-to-date'
+    if [ $? -gt 0 ]; then
+      echorun git commit -a -m "Update configuration" || return $?
+      echorun git push || return $?
+    fi
+    cd - || return $?
+  fi
   if [ ! -d ~/.hammerspoon ]; then
     echorun git clone git@github.com:pr0d1r2/dothammerspoon.git ~/.hammerspoon --recurse-submodules || return $?
   else
