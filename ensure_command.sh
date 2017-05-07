f13428deaf7fcf0bba35e3dda296f1dfd64a46cc
Marcin Nowicki <pr0d1r2@gmail.com>
2017-05-07 05:42:22 +0200

    Use proper namespace

--- ensure_command.sh
+++ ensure_command.sh
@@ -5,20 +5,20 @@ function ensure_command() {
       echorun brew install `ensure_command_package $1` || return $?
       ;;
     *)
-      local ensure_command_brew_params_PACKAGE=`brew search $1 | grep "^$1$"`
-      case $ensure_command_brew_params_PACKAGE in
+      local ensure_command_PACKAGE=`brew search $1 | grep "^$1$"`
+      case $ensure_command_PACKAGE in
         "")
-          local ensure_command_brew_params_PACKAGE=`brew cask search $1 | grep "^$1$"`
-          case $ensure_command_brew_params_PACKAGE in
+          local ensure_command_PACKAGE=`brew cask search $1 | grep "^$1$"`
+          case $ensure_command_PACKAGE in
             "")
               ;;
             *)
-              echorun brew cask install $ensure_command_brew_params_PACKAGE || return $?
+              echorun brew cask install $ensure_command_PACKAGE || return $?
               ;;
           esac
           ;;
         *)
-          echorun brew install $ensure_command_brew_params_PACKAGE || return $?
+          echorun brew install $ensure_command_PACKAGE || return $?
           ;;
       esac
       ;;
