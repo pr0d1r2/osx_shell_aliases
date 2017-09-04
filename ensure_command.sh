c8e63727974b701b3112bd0b6112f9b2c23292aa
Marcin Nowicki <pr0d1r2@gmail.com>
2017-09-04 06:47:39 +0200

    Allow to install multiple commands at once

--- ensure_command.sh
+++ ensure_command.sh
@@ -1,33 +1,37 @@
 function ensure_command() {
-  which "$(echo "$1" | cut -f 1 -d @)" &>/dev/null && return 0
-  case $1 in
-    aws | gpg | postgres | magick | socksify)
-      echorun brew install "$(ensure_command_package "$1")" || return $?
-      ;;
-    *)
-      local ensure_command_PACKAGE
-      ensure_command_PACKAGE=$(brew search "$1" | grep "^$1$")
-      case $ensure_command_PACKAGE in
-        "")
-          local ensure_command_PACKAGE
-          ensure_command_PACKAGE=$(brew cask search "$1" | grep "^$1$")
-          case $ensure_command_PACKAGE in
-            "")
-              ;;
-            *)
-              echorun brew cask install "$ensure_command_PACKAGE" || return $?
-              ;;
-          esac
-          ;;
-        *)
-          echorun brew install "$ensure_command_PACKAGE" || return $?
-          ;;
-      esac
-      ;;
-  esac
-  case $1 in
-    *@*)
-      brew link "$1" --force || return $?
-      ;;
-  esac
+  local ensure_command_NAME
+  for ensure_command_NAME in "$@"
+  do
+    which "$(echo "$ensure_command_NAME" | cut -f 1 -d @)" &>/dev/null && return 0
+    case $ensure_command_NAME in
+      aws | gpg | postgres | magick | socksify)
+        echorun brew install "$(ensure_command_package "$ensure_command_NAME")" || return $?
+        ;;
+      *)
+        local ensure_command_PACKAGE
+        ensure_command_PACKAGE=$(brew search "$ensure_command_NAME" | grep "^$ensure_command_NAME$")
+        case $ensure_command_PACKAGE in
+          "")
+            local ensure_command_PACKAGE
+            ensure_command_PACKAGE=$(brew cask search "$ensure_command_NAME" | grep "^$ensure_command_NAME$")
+            case $ensure_command_PACKAGE in
+              "")
+                ;;
+              *)
+                echorun brew cask install "$ensure_command_PACKAGE" || return $?
+                ;;
+            esac
+            ;;
+          *)
+            echorun brew install "$ensure_command_PACKAGE" || return $?
+            ;;
+        esac
+        ;;
+    esac
+    case $ensure_command_NAME in
+      *@*)
+        brew link "$ensure_command_NAME" --force || return $?
+        ;;
+    esac
+  done
 }
