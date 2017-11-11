871f8b513df8ac60991348cc50e5e99ba5ea0a37
Marcin Nowicki <pr0d1r2@gmail.com>
2017-11-11 03:51:06 +0100

    Minimize implementation

--- ensure_command_package.sh
+++ ensure_command_package.sh
@@ -1,29 +1,13 @@
 function ensure_command_package() {
   case $1 in
-    aws)
-      echo awscli
-      ;;
-    7z)
-      echo p7zip
-      ;;
-    gpg)
-      echo gnupg
-      ;;
-    postgres)
-      echo postgresql
-      ;;
-    magick)
-      echo imagemagick
-      ;;
-    socksify)
-      echo dante
-      ;;
-    xml2-config)
-      echo libxml2
-      ;;
-    *)
-      echo "$1"
-      ;;
+    aws) echo awscli;;
+    7z) echo p7zip;;
+    gpg) echo gnupg;;
+    postgres) echo postgresql;;
+    magick) echo imagemagick;;
+    socksify) echo dante;;
+    xml2-config) echo libxml2;;
+    *) echo "$1";;
   esac
   return $?
 }
