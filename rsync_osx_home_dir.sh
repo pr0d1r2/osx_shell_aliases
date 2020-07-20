a1d9e02d2fbd2207c71d086be4476efdb56de693
Marcin Nowicki <pr0d1r2@gmail.com>
2020-07-20 09:18:19 +0200

    Prevent syncing not permitted directories

--- rsync_osx_home_dir.sh
+++ rsync_osx_home_dir.sh
@@ -1,15 +1,25 @@
 function rsync_osx_home_dir() {
   rsync $@ \
-    --exclude .Trashes \
-    --exclude Library/Mail \
-    --exclude Library/Caches \
-    --exclude Library/Containers/com.docker.docker \
-    --exclude Library/Developer \
-    --exclude Library/Logs \
-    --exclude "Library/Mobile Documents" \
-    --exclude .rbenv \
-    --exclude .vagrant.d \
+    --exclude ".Trashes" \
     --exclude "Library/Application Support" \
+    --exclude "Library/IdentityServices" \
+    --exclude "Library/Messages" \
+    --exclude "Library/HomeKit" \
+    --exclude "Library/Sharing" \
+    --exclude "Library/Mail" \
+    --exclude "Library/Safari" \
+    --exclude "Library/Suggestions" \
+    --exclude "Library/Suggestions" \
+    --exclude "Library/Containers" \
+    --exclude "Library/PersonalizationPortrait" \
+    --exclude "Library/Metadata" \
+    --exclude "Library/Cookies" \
+    --exclude "Library/Caches" \
+    --exclude "Library/Developer" \
+    --exclude "Library/Logs" \
+    --exclude "Library/Mobile Documents" \
+    --exclude ".rbenv" \
+    --exclude ".vagrant.d" \
 
   return $?
 }
