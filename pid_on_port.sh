27d3c0406848de299cfb0c10babcf0fd879772fc
Marcin Nowicki <pr0d1r2@gmail.com>
2020-10-12 13:55:43 +0200

    Show PIDs on given TCP ports in macOS

---
+++ pid_on_port.sh
@@ -0,0 +1,10 @@
+# Show PIDs on given TCP ports in macOS
+#
+# Example usage:
+#   on_port 3000 4000
+# Returns:
+#   8472
+#   6969
+function pid_on_port() {
+  parallel "lsof -nP -iTCP:{} | grep LISTEN | awk '{ print \$2 }'" ::: $@
+}
